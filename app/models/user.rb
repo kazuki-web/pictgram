22a62883f5669f5f891d53d5dadc3cfb28ceade1
kazuki <adress>
2023-01-10 05:06:17 +0000

    second touch

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { minimum: 1,maximum: 15}
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, length: { minimum: 8, maximum: 32 }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
   
   has_secure_password
-  
-  
-  has_many :topics
-  has_many :favorites
-  has_many :favorite_topics, through: :favorites, source: 'topic'
   
 end
